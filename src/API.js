f48702a6ca4bec32e128ca93cad57ff0859ef128
John <Baozi960101@gmail.com>
2021-09-07 16:57:27 +0800

    2021/09/07

--- src/API.js
+++ src/API.js
@@ -1,10 +1,32 @@
 const API = "http://food.argus.work/venus/index.php/GetData/getData";
-export const FoodApi =
-  "https://argus.work/argus/public/api/argus?key=美食&start_date=2021-08-30&end_date=2021-09-01&crawler_Web=ETtoday,Ptt,Dcard,Ctee,Chinatimes,Udn,Storm,Mirrormedia,Newtalk";
-export const TravelApi =
-  "https://argus.work/argus/public/api/argus?key=旅遊&start_date=2021-08-30&end_date=2021-09-01&crawler_Web=ETtoday,Ptt,Dcard,Ctee,Chinatimes,Udn,Storm,Mirrormedia,Newtalk";
-export const RestaurantApi =
-  "https://argus.work/argus/public/api/argus?key=餐廳&start_date=2021-08-30&end_date=2021-09-01&crawler_Web=ETtoday,Ptt,Dcard,Ctee,Chinatimes,Udn,Storm,Mirrormedia,Newtalk";
+
+function getDay(day) {
+  let today = new Date();
+  let targetday_milliseconds = today.getTime() + 1000 * 60 * 60 * 24 * day;
+  today.setTime(targetday_milliseconds); //關鍵代碼
+
+  let tYear = today.getFullYear();
+  let tMonth = today.getMonth();
+  let tDate = today.getDate();
+  tMonth = doHandleMonth(tMonth + 1);
+  tDate = doHandleMonth(tDate);
+  return tYear + "-" + tMonth + "-" + tDate;
+}
+function doHandleMonth(month) {
+  let m = month;
+  if (month.toString().length === 1) {
+    m = "0" + month;
+  }
+  return m;
+}
+
+let nowDate = getDay(0);
+
+export const FoodApi = `https://argus.work/argus/public/api/argus?key=美食&start_date=2021-08-30&end_date=${nowDate}&crawler_Web=ETtoday,Ptt,Dcard,Ctee,Chinatimes,Udn,Storm,Mirrormedia,Newtalk`;
+
+export const TravelApi = `https://argus.work/argus/public/api/argus?key=旅遊&start_date=2021-08-30&end_date=${nowDate}&crawler_Web=ETtoday,Ptt,Dcard,Ctee,Chinatimes,Udn,Storm,Mirrormedia,Newtalk`;
+
+export const RestaurantApi = `https://argus.work/argus/public/api/argus?key=餐廳&start_date=2021-08-30&end_date=${nowDate}&crawler_Web=ETtoday,Ptt,Dcard,Ctee,Chinatimes,Udn,Storm,Mirrormedia,Newtalk`;
 
 export const ArticleNumber = (number) => {
   let fd = new FormData();
